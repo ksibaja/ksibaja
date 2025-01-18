b9d4fc6c1bc77d00913098d677075a448421fc90
ksibaja <rslk0325@gmail.com>
2025-01-17 18:52:00 -0600

    Creating routes, main pages and components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    title: 'Home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    title: 'About',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    title: 'Contact',
+    component: ContactComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    title: 'Not Found',
+    component: NotFoundComponent
+  },
+];
