c6b95e75b864b1f45ef6f195b5d1fa6c681ec4f2
ksibaja <rslk0325@gmail.com>
2025-01-23 16:03:51 -0600

    email service, loading, environment config files

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import Swal from 'sweetalert2'
-
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-contact',
   imports: [],
@@ -8,21 +9,46 @@ import Swal from 'sweetalert2'
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  loading: boolean = false;
 
-  sendEmail() {
+  sendEmail(e: Event) {
+    //let timerInterval: any;
     Swal.fire({
-      title: "Good job!",
-      text: "Email sent successfully!",
-      icon: "success"
-    });
+      customClass: {},
+      width: '100px',
+      didOpen: () => {
+       Swal.showLoading();
+     },
+   });
 
-    try {
-      const myForm: any = document.getElementById("myForm");
-      myForm.reset();
-    } catch (error) {
-      console.error("Error: ", error);
-    }
+    e.preventDefault();
+    //this.loading = true;
+    emailjs
+      .sendForm(environment.SERVICE_ID, environment.TEMPLATE_ID, e.target as HTMLFormElement, {
+        publicKey: environment.PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          //this.loading = false;
 
-    return false;
+          Swal.close();
+          console.log('SUCCESS!');
+          Swal.fire({
+            title: "Good job!",
+            text: "Email sent successfully!",
+            icon: "success"
+          });
+
+          try {
+            const myForm: any = document.getElementById("myForm");
+            myForm.reset();
+          } catch (error) {
+            console.error("Error: ", error);
+          }
+        },
+        (error) => {
+          console.log('FAILED...', (error as EmailJSResponseStatus).text);
+        },
+      );
   }
 }
